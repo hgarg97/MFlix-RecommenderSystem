c9efa06f1926137236ca5252ef315f5802c23e76
hgarg97 <hgarg97@gmail.com>
2024-04-16 23:32:50 -0500

    Movie Details Page Integration

--- src/mflix-app/app.py
+++ src/mflix-app/app.py
@@ -1,7 +1,7 @@
 # app.py
-
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
+from datetime import datetime
 
 app = Flask(__name__)
 
@@ -19,11 +19,51 @@ movies_df = pd.read_excel('static/movies_data.xlsx')
 # Define a function to fetch movie details by movieId
 def get_movie_details(movie_id):
     movie_details = movies_df[movies_df['movieId'] == movie_id].iloc[0]
-    return {
+
+    # Extract genres names
+    genres = eval(movie_details['genres'])
+    genre_names = [genre['name'] for genre in genres]
+    genres_str = ', '.join(genre_names)
+
+    # Extract cast names and image links
+    cast_info = eval(movie_details['cast'])
+    cast_names = [actor['name'] for actor in cast_info]
+    top_5_cast = cast_names[:5]
+    cast_str = ', '.join(top_5_cast)
+
+
+    # Format release date
+    rel_date = movie_details['release_date']
+    release_date = rel_date.strftime("%d %b %Y")
+
+    # Convert runtime to hour and minute format
+    runtime_hours = movie_details['runtime'] // 60
+    runtime_minutes = movie_details['runtime'] % 60
+    runtime = f"{runtime_hours}h {runtime_minutes}m"
+
+    # Create a dictionary with all the extracted information
+    movies_info = {
+        'movieId' : movie_details['movieId'],
         'title': movie_details['title'],
-        'poster_working_url': movie_details['poster_working_url']
-        # Add more fields as needed
+        'poster_working_url': movie_details['poster_working_url'],
+        'original_language': movie_details['original_language'],
+        'overview': movie_details['overview'],
+        'popularity': movie_details['popularity'],
+        'budget': movie_details['budget'],
+        'genres': genres_str,
+        'production_companies': movie_details['production_companies'],
+        'production_countries': movie_details['production_countries'],
+        'release_date': release_date,  # Formatted release date
+        'revenue': movie_details['revenue'],
+        'runtime': runtime,  # Runtime in hours and minutes format
+        'spoken_languages': movie_details['spoken_languages'],
+        'vote_average': movie_details['vote_average'],
+        'vote_count': movie_details['vote_count'],
+        'keywords': movie_details['keywords'],
+        'cast': cast_str,
+        'crew': movie_details['crew']
     }
+    return movies_info
 
 
 @app.route('/')
@@ -54,6 +94,12 @@ def home():
             return render_template('login.html', error_message=error_message)    
     else:
         return redirect(url_for('login'))
+
+@app.route('/movie/<int:movie_id>')
+def movie_details(movie_id):
+    movie = get_movie_details(movie_id)
+    return render_template('movie_details.html', movie=movie)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
