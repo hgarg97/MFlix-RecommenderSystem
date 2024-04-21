774ad8138760efffd29419e3f2a0d43f69a9fa60
hgarg97 <hgarg97@gmail.com>
2024-04-20 21:19:36 -0500

    Search and Image2vec Integration with UI

--- src/mflix-app/app.py
+++ src/mflix-app/app.py
@@ -2,6 +2,13 @@
 from flask import Flask, render_template, request, redirect, url_for
 import pandas as pd
 from datetime import datetime
+import json
+import numpy as np
+import string
+import pickle
+import io
+import torch
+from sentence_transformers import SentenceTransformer
 
 app = Flask(__name__)
 
@@ -13,8 +20,43 @@ users = {
 
 authenticated = False
 
+movie_ids_your5 = [2, 134234, 20, 96079, 748]  # Example movie IDs
+movie_ids_rec5 = [3450, 3629, 5096, 5720, 58559]
+
 # Load data from Excel file into a pandas DataFrame
 movies_df = pd.read_csv('static/movies_data3200.csv')
+
+# Loading Image2Vec Similarity JSON 
+with open('static/sorted_similarity.json', 'r') as file:
+    sorted_similarity = json.load(file)
+
+posters_like_this_dict = {}
+for key, value in sorted_similarity.items():
+    posters_like_this_dict[int(key)] = value[:5]
+
+# Integrating DistilBert with Search
+with open('static/encodings_description.pickle','rb') as file:
+    encoded_data = pickle.load(file)
+
+class CPU_Unpickler(pickle.Unpickler):
+    def find_class(self, module, name):
+        if module == 'torch.storage' and name == '_load_from_bytes':
+            return lambda b: torch.load(io.BytesIO(b), map_location='cpu')
+        else:
+            return super().find_class(module, name)
+        
+with open('static/model.pickle','rb') as file:
+    model = CPU_Unpickler(file).load()
+
+def find_similar_items(query: str, metadata: pd.DataFrame, model: SentenceTransformer = model, encodings: np.array = encoded_data, top_n: int = 5):
+    query_vector = model.encode([query])
+    # print(query_vector)
+    similarity = np.dot(encodings,query_vector.T)
+    # print(similarity)
+    top_items = similarity.flatten().argsort()[-top_n:][::-1]
+    # print(top_items)
+    # return metadata['asin'].iloc[top_items]
+    return list(metadata['movieId'].iloc[top_items].values)
 
 # Define a function to fetch movie details by movieId
 def get_movie_details(movie_id):
@@ -66,19 +108,6 @@ def get_movie_details(movie_id):
     }
     return movies_info
 
-more_like_this_dict = {
-                2: [156187, 156503, 157573, 158540, 157677],
-                134234: [158338, 158135, 157106, 158597, 158904],
-                20: [158338, 158579, 158286, 158542, 157573],
-                96079: [157573, 156303, 156675, 157166, 156187],
-                748: [161326, 156303, 156675, 157166, 156187],
-                3450: [158286, 158579, 156675, 156187, 158540],
-                3629: [156187, 156503, 158540, 158286, 156675],
-                5096: [158135, 157106, 158597, 158904, 156503],
-                5720: [156503, 157106, 158597, 158135, 156675],
-                58559: [158338, 158579, 158286, 158542, 157573]
-                }
-
 @app.route('/')
 def login():
     return render_template('login.html')
@@ -92,17 +121,8 @@ def home():
         password = request.form['password']
         if email in users and users[email] == password:
             authenticated = True
-            # Assuming you have a list of movie IDs you want to display on the home page
-            movie_ids_your5 = [2, 134234, 20, 96079, 748]  # Example movie IDs
-            movie_ids_rec5 = [3450, 3629, 5096, 5720, 58559]  # Another set of example movie IDs
-            
-            # Fetch movie details for each set of movie IDs
-            # movies_data_your5 = [get_movie_details(movie_id) for movie_id in movie_ids_your5]
-            # movies_data_rec5 = [get_movie_details(movie_id) for movie_id in movie_ids_rec5]
 
-            
-            # Pass both sets of movie data to the template for rendering
-            return render_template('home.html', authenticated=authenticated, movie_ids_your5=movie_ids_your5, movie_ids_rec5=movie_ids_rec5, more_like_this_dict = more_like_this_dict, get_movie_details=get_movie_details)
+            return render_template('home.html', authenticated=authenticated, movie_ids_your5=movie_ids_your5, movie_ids_rec5=movie_ids_rec5, posters_like_this_dict = posters_like_this_dict, get_movie_details=get_movie_details)
         else:
             error_message = "Incorrect email or password. Please try again."
             return render_template('login.html', error_message=error_message)    
@@ -112,7 +132,13 @@ def home():
 @app.route('/movie/<int:movie_id>')
 def movie_details(movie_id):
     movie = get_movie_details(movie_id)
-    return render_template('movie_details.html', movie=movie, more_like_this_dict=more_like_this_dict, get_movie_details=get_movie_details)
+    return render_template('movie_details.html', movie=movie, posters_like_this_dict=posters_like_this_dict, get_movie_details=get_movie_details)
+
+@app.route('/search', methods=['POST'])
+def search_movies():
+    search_query = request.form['search_query']
+    search_results = find_similar_items(search_query, movies_df)
+    return render_template('home.html', authenticated=authenticated, movie_ids_your5=movie_ids_your5, movie_ids_rec5=movie_ids_rec5, posters_like_this_dict=posters_like_this_dict, search_results=search_results, get_movie_details=get_movie_details)
 
 
 if __name__ == '__main__':
